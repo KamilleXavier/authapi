f58f1abda6e7fd7e81450563d30930632d705920
KamilleXavier <vkamille239@email.com>
2026-03-29 23:44:27 -0300

    feat: cria repository de usuário

---
+++ src/main/java/com/security/authapi/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.security.authapi.repository;
+
+import com.security.authapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
