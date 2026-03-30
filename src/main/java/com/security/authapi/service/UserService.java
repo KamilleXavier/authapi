5c4b0d3f77ab4336ce83ffdb38285e17370ea209
KamilleXavier <vkamille239@email.com>
2026-03-30 00:07:19 -0300

    feat: cria service de usuário

---
+++ src/main/java/com/security/authapi/service/UserService.java
@@ -0,0 +1,17 @@
+package com.security.authapi.service;
+
+import com.security.authapi.model.User;
+import com.security.authapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User register(User user) {
+        return userRepository.save(user);
+    }
+}
